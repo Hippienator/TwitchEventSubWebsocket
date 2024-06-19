ade645bf960a6857f684828a5215edce6b605b04
Hippienator <jacob@observator.dk>
2024-06-19 04:24:51 +0200

    Changed conditions to be a dictionary.

--- TwitchEventSubWebsocket/Types/Event/RevocationEventArgs.cs
+++ TwitchEventSubWebsocket/Types/Event/RevocationEventArgs.cs
@@ -13,16 +13,32 @@ namespace Hipbotnator.TwitchEventSubWebsocket.Types.Event
         /// The ID of the subscription being revoked
         /// </summary>
         public string SubscriptionID { get; }
+
+        /// <summary>
+        /// The reason the subscription was revoked. Options are:
+        /// - authorization_revoked — The user in the condition object revoked the authorization that let you get events on their behalf.
+        /// - user_removed — The user in the condition object is no longer a Twitch user.
+        /// - version_removed — The subscribed to subscription type and version is no longer supported.
+        /// </summary>
         public string Status { get; }
+
+        /// <summary>
+        /// Type of the revoked subscription.
+        /// </summary>
         public string Type { get; }
-        public Conditions Conditions { get; }
+
+        /// <summary>
+        /// Conditions of the revoked subscription.
+        /// </summary>
+        public Dictionary<string,string> Conditions { get; } = new Dictionary<string,string>();
 
         public RevocationEventArgs (string subscriptionID, string status, string type, JObject conditions)
         {
             SubscriptionID = subscriptionID;
             Status = status;
             Type = type;
-            Conditions = new Conditions(conditions);
+            foreach (KeyValuePair<string, JToken?> token in conditions)
+                Conditions.Add(token.Key, token.ToString());
         }
     }
 }
